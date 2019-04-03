d39bfa43384df4d0da9a83644f5e75d6c8c165e1
Yana5417 <1404578601@qq.com>
2019-04-03 16:37:42 +0800

    model hook

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | list rentals', function(hooks) {
@@ -8,14 +8,22 @@ module('Acceptance | list rentals', function(hooks) {
   // test('should show rentals as then home page', async function (assert) {
   // });
 
-  // test('should link to information about the company.', async function (assert) {
-  // });
+  test('should link to information about the company.', async function (assert) {
+    await visit('/');
+    await click('.menu-about');
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
 
-  // test('should link to contact information.', async function (assert) {
-  // });
+  test('should link to contact information.', async function (assert) {
+    await visit('/');
+    await click('.menu-contact');
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
 
-  // test('should list available rentals.', async function (assert) {
-  // });
+  test('should list available rentals.', async function (assert) {
+    await visit('/');
+    assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
+  });
 
   // test('should filters the list of rentals by city.', async function (assert){
   // });
@@ -25,6 +33,6 @@ module('Acceptance | list rentals', function(hooks) {
 
   test('visiting /', async function(assert) {
     await visit('/');
-    assert.equal(currentURL(), '/');
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
   });
 });
